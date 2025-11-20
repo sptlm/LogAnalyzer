8acb36182a10112b5b932305920fb13d11d99939
Spirit <Leonidmaslennikov39890@gmail.com>
2025-11-20 21:29:18 +0300

    FormatUtil

---
+++ src/main/java/academy/util/FormatUtils.java
@@ -0,0 +1,21 @@
+package academy.util;
+
+public final class FormatUtils {
+
+    private FormatUtils() {
+    }
+
+    public static String formatNumber(int number) {
+        return String.format("%,d", number).replace(",", "_");
+    }
+
+    public static String formatBytes(double bytes) {
+        if (bytes < 1024) {
+            return String.format("%.0fb", bytes);
+        } else if (bytes < 1024 * 1024) {
+            return String.format("%.2fkb", bytes / 1024);
+        } else {
+            return String.format("%.2fmb", bytes / (1024 * 1024));
+        }
+    }
+}
